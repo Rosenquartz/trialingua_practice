069282f52035e8bb3922e64412539c7fc091c4f2
TayanAir <tayangelera@gmail.com>
2022-06-10 14:19:35 +0800

    Started on user.controller

--- newApp/controllers/user.controller.js
+++ newApp/controllers/user.controller.js
@@ -1,59 +1,84 @@
-const shortid = require('shortid')
+const controller = {
+    createUser: function(req, res) {
+        // Check if any of the fields are missing.
+        if (!req.body.username || !req.body.password || !req.body.email) {
+            res.status(400);
+            res.end(JSON.stringify({
+                error: "Insufficient information."
+            }));
+        }
 
+        // Check if username is in use.
+        if (req.body.username in userList) {
+            res.status(400);
+            res.end(JSON.stringify({
+                error: "Username is already in use."
+            }));
+        }
+
+        // Check if email is in use.
+        if (req.body.email in emailList) {
+            res.status(400);
+            res.end(JSON.stringify({
+                error: "Email is already in use."
+            }));
+        }
+
+        // Check email format.
+        var regexp = /\S+@\S+\.\S+/;
+        if (!regexp.test(req.email)) {
+            res.status(400);
+            res.end(JSON.stringify({
+                error: "Invalid email format."
+            }))
+        }
+
+        // Otherwise, everything checks out; next step is to make user.
+        userList[req.body.username] = new User(req.body.username, req.body.password, req.body.email);
+        emailList.push(req.body.email)
+        res.status(200);
+        res.end(JSON.stringify({
+            newUser: req.body
+        }))
+    },
+    updateUser: function(req, res) {
+        const user = req.params.userId;
+        for (var field in req.body) {
+            console.log(field)
+        }
+    },
+    getUserList: function(req, res) {
+        let userArray = Object.keys(userList)
+        res.status(200).json({
+            users: userArray // array of 
+        })
+    },
+    getUserProfile: function(req, res) {
+
+    },
+    getUserProgress: function(req, res) {
+
+    },
+    getModuleQuestions: function(req, res) {
+        moon: 가사
+    }
+}
+
+// Temporary class for testing.
 class User {
-    constructor (username, password, languages, sid) {
+    constructor (username, password, email) {
         this.username = username,
         this.password = password,
-        this.languages = languages,
-        this.sid = sid
+        this.email = email,
+        this.preferred_language = null
     }
 }
 
 var userList = {}
-var shortIdList = {}
-
-const controller = {
-    postUser: function(req, res) {
-        if (!req.body.username || !req.body.password || !req.body.languages) {
-            console.log('Empty Username or Password');
-            res.status(400);
-            res.end(JSON.stringify({
-                error: "Insufficient info."
-            }))
-        }
-        if (req.body.username in userList) {
-            res.status(400);
-            res.end(JSON.stringify({
-                error: "Existing user."
-            }))
-        }
-        //step 3 verify email
-        newShortId = shortid.generate().toLowerCase();
-        shortIdList[newShortId] = req.body.username;
-        userList[req.body.username] = new User(req.body.username, req.body.password, req.body.languages,newShortId);
-        res.status(200);
-        res.end(JSON.stringify({
-            newUser: req.body,
-            sid: newShortId
-        }))
-
-    },
-    putUser: function(req, res) {
-        res.status(200).json({
-            print: 'yes'
-        })
-    },
-    getUsers: function(req, res) {
-        let userArray = [] //populate with items of userlist
-        res.status(200).json({
-            users: userList // array of 
-        })
-    }
-    //getUser
-}
-
-userA = new User('Chocolate', 'icecream', ['German'], 's7eup99')
-userList['Chocolate'] = userA
-shortIdList['choco'] = 'Chocolate'
+var emailList = {}
+userA = new User("marck27", "password1", "m27@gmail.com")
+userB = new User("benJ", "password2", "jamin@hotmail.coms")
+userList["marck27"] = userA
+userList["benJ"] = userB
 
 module.exports = controller;
--- newApp/routes/users.js
+++ newApp/routes/users.js
@@ -2,15 +2,8 @@ var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/user.controller')
 
-/* GET users listing. */
-//router.get('/', function(req, res, next) {
-//  res.send('respond with a resource');
-  
-//});
+router.get('/', controller.getUserList);
+router.post('/newUser', controller.createUser);
+router.put('/:userId', controller.getUserProfile);
 
-router.get('/', controller.getUsers)
-router.put('/:userId', controller.putUser);
-router.post('/newUser', controller.postUser)
-
-console.log('in users')
 module.exports = router;
